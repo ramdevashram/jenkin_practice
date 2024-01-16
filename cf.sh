394c0f85cbf0c67471c2fddc2cc34ac749a532a5
EC2 Default User <ec2-user@ip-172-31-21-49.ec2.internal>
2024-01-16 15:19:08 +0000

    delete

--- cf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "we are going to create a folder NEW"
-mkdir new
-echo "we are go inside the folder new"
-cd new
-echo "we are creating a folder insider folder SUB"
-mkdir sub
-echo "go inside the folder"
-cd sub
